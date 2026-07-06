f0a10895b32acda72bc06d38042713d43e7f1595
nommuu <edisonvillar10@gmail.com>
2026-07-06 14:15:35 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Villar, Edison Pacquiao and BSIT - 3A</Text>
+      <Text>Mobile App Development.</Text>
+    </View>
+  );
+}
